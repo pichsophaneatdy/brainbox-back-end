3dfc23c91b1b81b9fc1c9bc30255ef8701d1302f
Neat <sophaneat@student.fdu.edu>
2023-06-14 22:58:06 -0700

    Set up connection to database

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./database/connectDB");
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+
+
+// Connect to Database abd start the server
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_DB_URL);
+        app.listen(PORT, () => console.log(`Connected to DB and server is running on PORT ${PORT}`))
+    } catch(error) {
+        console.log(error)
+    }
+}
+start();
