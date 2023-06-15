ba3f4f9831f088efae24d8042927b956172dce66
Neat <sophaneat@student.fdu.edu>
2023-06-14 23:19:09 -0700

    Add User Schema

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,1 +1,38 @@
 const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        require: true,
+        min: 3
+    },
+    lastName: {
+        type: String,
+        require: true,
+        min: 3
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    picturePath: {
+        type: String,
+        default: ""
+    },
+    university: {
+        type: String,
+        default: ""
+    },
+    degree: {
+        type: String,
+        default: ""
+    },
+    occupation: {
+        type: String,
+        default: "Student"
+    }
+})
